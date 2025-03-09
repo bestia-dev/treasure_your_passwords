7c452be0bcb60c785f01b6aa4391e82300ade068
bestia.dev <info@bestia.dev>
2025-03-09 20:43:07 +0000

    update docs

--- docs/trait.impl/core/marker/trait.Sync.js
+++ docs/trait.impl/core/marker/trait.Sync.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["treasure",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"treasure/encrypt_decrypt_with_ssh_key_mod/struct.EncryptedTextWithMetadata.html\" title=\"struct treasure::encrypt_decrypt_with_ssh_key_mod::EncryptedTextWithMetadata\">EncryptedTextWithMetadata</a>",1,["treasure::encrypt_decrypt_with_ssh_key_mod::EncryptedTextWithMetadata"]]]]]);
+    var implementors = Object.fromEntries([["treasure_your_passwords",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"treasure_your_passwords/encrypt_decrypt_with_ssh_key_mod/struct.EncryptedTextWithMetadata.html\" title=\"struct treasure_your_passwords::encrypt_decrypt_with_ssh_key_mod::EncryptedTextWithMetadata\">EncryptedTextWithMetadata</a>",1,["treasure_your_passwords::encrypt_decrypt_with_ssh_key_mod::EncryptedTextWithMetadata"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[462]}
+//{"start":57,"fragment_lengths":[522]}
--- docs/trait.impl/serde/ser/trait.Serialize.js
+++ docs/trait.impl/serde/ser/trait.Serialize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["treasure",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.218/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"treasure/encrypt_decrypt_with_ssh_key_mod/struct.EncryptedTextWithMetadata.html\" title=\"struct treasure::encrypt_decrypt_with_ssh_key_mod::EncryptedTextWithMetadata\">EncryptedTextWithMetadata</a>"]]]]);
+    var implementors = Object.fromEntries([["treasure_your_passwords",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"treasure_your_passwords/encrypt_decrypt_with_ssh_key_mod/struct.EncryptedTextWithMetadata.html\" title=\"struct treasure_your_passwords::encrypt_decrypt_with_ssh_key_mod::EncryptedTextWithMetadata\">EncryptedTextWithMetadata</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[394]}
+//{"start":57,"fragment_lengths":[439]}
